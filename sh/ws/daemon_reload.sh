4928a9dae01aec690702014d0069d12e4c0b36d5
Mark Greenslade <momipsl@ipsl.jussieu.fr>
2018-05-15 15:46:43 +0200

    Daemon reload change

--- sh/ws/daemon_reload.sh
+++ sh/ws/daemon_reload.sh
@@ -6,8 +6,8 @@ source $PYESSV_LIB_HOME/sh/utils.sh
 # Main entry point.
 main()
 {
-	source $PYESSV_LIB_HOME/sh/daemon_stop.sh
-	source $PYESSV_LIB_HOME/sh/daemon_start.sh
+	source $PYESSV_LIB_HOME/sh/ws/daemon_stop.sh
+	source $PYESSV_LIB_HOME/sh/ws/daemon_start.sh
 }
 
 # Invoke entry point.
