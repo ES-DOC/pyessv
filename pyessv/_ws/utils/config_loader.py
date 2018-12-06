dfae3e7119b511503e6f12b78362752bda5b222a
Mark A. Greenslade <momipsl@ipsl.fr>
2018-12-06 14:56:43 -0500

    1. Made ws.conf optional.

--- pyessv/_ws/utils/config_loader.py
+++ pyessv/_ws/utils/config_loader.py
@@ -15,6 +15,7 @@ import json
 
 from pyessv._ws.utils.constants import HTTP_RESPONSE_SERVER_ERROR
 from pyessv._ws.utils.convertor import json_file_to_namedtuple
+from pyessv._ws.utils.convertor import to_namedtuple
 from pyessv._ws.utils.exceptions import ConfigFileNotFoundException
 
 
@@ -22,8 +23,17 @@ from pyessv._ws.utils.exceptions import ConfigFileNotFoundException
 # Default configuration file path.
 _CONFIG_FPATH = "ws.conf"
 
+# Default configuration.
+_DEFAULT = {
+    'cookie_secret': 'p2FAdrUN3tac',
+    'host': 'localhost',
+    'apply_security_policy': False,
+    'mode': 'dev',
+    'port': 5010
+}
+
 # Configuration data.
-data = None
+data = to_namedtuple(_DEFAULT)
 
 
 def _get_config_fpath(config_path):
@@ -33,10 +43,8 @@ def _get_config_fpath(config_path):
     path = os.getenv('PYESSV_LIB_HOME')
     path = os.path.join(path, "ops/config")
     path = os.path.join(path, _CONFIG_FPATH)
-    if not os.path.exists(path):
-        raise ConfigFileNotFoundException(fpath)
-
-    return path
+    if os.path.exists(path):
+        return path
 
 
 def _init():
@@ -45,13 +53,12 @@ def _init():
     """
     global data
 
-    # Get configuration file path (falling back to template if necessary).
     fpath = _get_config_fpath('ws.conf')
-
-    # Convert config file to a named tuple.
-    data = json_file_to_namedtuple(fpath)
-
-    logger.log_web("Configuration file loaded @ {}".format(fpath))
+    if fpath is not None:
+        data = json_file_to_namedtuple(fpath)
+        logger.log_web("Config file loaded @ {}".format(fpath))
+    else:
+        logger.log_web_warning("Web-service config file not found: reverted to default")
 
 
 # Auto-initialize.
