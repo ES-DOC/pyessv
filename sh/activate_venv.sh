4c8217245c05f9eeb223a42c174e5d30c666e10c
Mark A. Greenslade <mark.greenslade@ieee.org>
2019-09-26 10:25:36 +0200

    1. Logging venv activation.

--- sh/activate_venv.sh
+++ sh/activate_venv.sh
@@ -9,6 +9,7 @@ main()
 	export PYTHONPATH=$PYESSV_LIB_HOME:$PYTHONPATH
 	venv_path=${PYESSV_LIB_VENV:-$PYESSV_LIB_HOME/ops/venv}
 	source $venv_path/bin/activate
+	log "venv activated @ "$venv_path
 }
 
 # Invoke entry point.
