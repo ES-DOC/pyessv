a929bea7515b5cf03c94a3ad6b192377252a7994
Mark A. Greenslade <asladeofgreen@pm.me>
2022-08-16 19:41:13 +0200

    1. Simplified parser configuration generator job.

--- jobs/set_parser_config/set_dataset_id.py
+++ jobs/set_parser_config/set_dataset_id.py
@@ -12,43 +12,26 @@ def get_config(a: Authority, s: Scope, template_raw: str):
     :param template_raw: A raw dataset id parsing template.
     
     """
-    # Destructure raw template into prefix and slots.
-    prefix, slots = _parse_template(a, s, template_raw)
+    # Set slots, i.e. set of vocabulary nodes embedded in the template.
+    slots: typing.List[str] = \
+        [f"{i[2:-2]}".replace("_", "-") for i in template_raw.split(".")[1:]]
 
-    # Set pyessv template.
+    # Set slot overrides.
+    if s.namespace == "wcrp:cmip6":
+        slots[0] = "activity-id"
+
+    # Set template prefix.
+    if s.namespace in ("ecmwf:cc4e", "wcrp:cmip6"):
+        prefix = s.canonical_name.upper()
+    else:
+        prefix = s.canonical_name
+
+    # Set template.
     template: str = f"{prefix}." + ".".join(["{}" for i in slots])
 
-    # Strip surrounding interpolation instruction.
-    slots: typing.List[str] = [f"{i[2:-2]}".replace("_", "-") for i in slots]
-
-    # Perform scope level parsing.
-    _parse_slots(s, slots)
-
-    # Transform slots so that they are valid pyessv collection namespaces.
-    collections: typing.List[str] = [f"{s}:{i}" for i in slots]
-
     return {
-        "prefix": prefix,
         "template": template,
         "template_raw": template_raw,
         "seperator": ".",
-        "collections": collections
+        "collections": [f"{s}:{i}" for i in slots]
     }
-
-
-def _parse_template(a: Authority, s: Scope, template_raw: str) -> typing.Tuple[str, typing.List[str]]:
-    """Returns 2 member tuple consisting of a template prefix plus collection references.
-    
-    """
-    if s.namespace in ("ecmwf:cc4e", "wcrp:cmip6"):
-        return s.canonical_name.upper(), template_raw.split(".")[1:]
-
-    return s.canonical_name, template_raw.split(".")[1:]
-
-
-def _parse_slots(s: Scope, slots: typing.List[str]):
-    """Parses template slots modifying as appropriate.
-    
-    """
-    if s.namespace == "wcrp:cmip6":
-        slots[0] = "activity-id"
